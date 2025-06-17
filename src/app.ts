296988f76dd88add3c26cce2f41fe79175571115
md.munna <mk8761174@gmail.com>
2025-06-17 10:15:37 +0600

    17-2 Setting Up Mongoose in Note App Project

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send("Welcome to Note App")
+})
+export default app;
